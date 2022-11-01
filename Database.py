19771c88825beaf38bd82a9a87cdb14dc8087df2
Julio R.R <juliorivera.2092@gmail.com>
2022-10-31 20:42:10 -0500

    Entrena y valida en una sola ejecución

--- Database.py
+++ Database.py
@@ -1,22 +1,10 @@
 #------------------------------Se importan las librerias -----------------------
+from time import sleep
 from xml.etree.ElementTree import tostring
 import cv2
 import Informacion_manos as im #Programa el cual contiene la deteccion y seguimiento de manos
 import tts as voz
 import os
-
-#---------------------------------------Se crea la carpeta donde se almacenara el entrenamiento ----------------
-nombre = input('Ingrese el nombre del objeto: ')
-voz.crearaudio(nombre)
-nombre = nombre.replace(" ", "")
-direccion = 'Fotos/Entrenamiento'
-carpeta = direccion + '/' + nombre
-if not os.path.exists(carpeta):
-    print('carpeta creada: ', carpeta)
-    os.makedirs(carpeta)
-
-#Se asigna un contador para el nombre de la fotos
-cont = 0
 
 #---------------------------------- Declaracion de variables -------------------------
 anchocam, altocam = 640, 480
@@ -32,27 +20,50 @@ cap.set(4,altocam)
 detector = im.detectormanos(maxManos=1, Confdeteccion=0.7)  #Solo se utilizara una mano
 
 
-while True:
-    #------------------------------------ Vamos a encontrar los puntos de la mano---------------------
-    ret, frame = cap.read()
-    manos = detector.encontrarmanos(frame) # Encontramos las manos
-    lista, bbox = detector.encontrarposicion(frame) # Se muestra las posiciones
-    if len(lista) != 0:
-        x1 = bbox[0]
-        y1 = bbox[1]
-        x2 = bbox[2]
-        y2 = bbox[3]
-        data = frame[y1:y2,x1:x2]
-        obje = cv2.resize(data, (200,200), interpolation=cv2.INTER_CUBIC) # Se redimenciona las fotos
-        cv2.imwrite(carpeta + "/Objeto_{}.jpg".format(cont), obje)
-        cont = cont + 1
+sleep(1)
+nombre = input('Ingrese el nombre del objeto: ')
+sleep(3)
+
+def entrenar(tipo,cont):
+    #---------------------------------------Se crea la carpeta donde se almacenara el entrenamiento ----------------
+    global anchocam, altocam, cap, detector, nombre
+    voz.crearaudio(nombre)
+    nombre = nombre.replace(" ", "")
+    direccion = 'Fotos/' + tipo
+    carpeta = direccion + '/' + nombre
+    if not os.path.exists(carpeta):
+        print('carpeta creada: ', carpeta)
+        os.makedirs(carpeta)
+    
+    while True:
+        #------------------------------------ Vamos a encontrar los puntos de la mano---------------------
+        ret, frame = cap.read()
+        manos = detector.encontrarmanos(frame) # Encontramos las manos
+        lista, bbox = detector.encontrarposicion(frame) # Se muestra las posiciones
+        if len(lista) != 0:
+            x1 = bbox[0]
+            y1 = bbox[1]
+            x2 = bbox[2]
+            y2 = bbox[3]
+            data = frame[y1:y2,x1:x2]
+            obje = cv2.resize(data, (200,200), interpolation=cv2.INTER_CUBIC) # Se redimenciona las fotos
+            cv2.imwrite(carpeta + "/Objeto_{}.jpg".format(cont), obje)
+            cont = cont + 1
 
 
 
 
-        cv2.imshow("Base Datos", frame)
-        k = cv2.waitKey(1)
-        if k == 27 or cont >= 300:
-            break
+            cv2.imshow("Base Datos", frame)
+            k = cv2.waitKey(1)
+            if k == 27 or cont >= 300:
+                break
+
+print("Entrenando...")
+entrenar('Entrenamiento',0)
+print("Entrenamiento terminado. \nValidando...")
+entrenar('Validacion', 0)
+print("Validacion terminada.")
+
+
 cap.release()
 cv2.destroyAllWindows()
